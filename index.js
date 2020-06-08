5b778fdf15c34b147f673387078c2df5624ae3b4
marshall405 <marshall.slemp@gmail.com>
2020-06-08 09:18:13 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+function superbowlWin(arr) {
+  let superBowl = arr.find(game => game.result == 'W')
+  if (superBowl) {
+    return superBowl.year
+  } else {
+    return superBowl
+  }
+}
